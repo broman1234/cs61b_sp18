6f2e3d204206b2ea7ebdd1ed3447f40e034dc460
Amanda <manmo431.gmail.com>
2021-11-30 02:56:24 +0000

    finished project0

--- proj0/TestPlanet.java
+++ proj0/TestPlanet.java
@@ -1,4 +1,5 @@
 public class TestPlanet {
+    /* Test pairwise force between two planets */
 	public static void main(String[] args) {
 		Planet p1 = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
 		Planet p2 = new Planet(3.0, 3.0, 0.0, 0.0, 10.0, "sun.gif");
