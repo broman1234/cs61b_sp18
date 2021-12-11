eaae34f983651d93ea8b1e94308771d99b8418ae
Amanda <manmo431.gmail.com>
2021-12-11 06:47:17 +0000

    simplified ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -14,6 +14,14 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
+    private boolean timeToIncreaseSize() {
+        return addNextFirst == addNextLast;
+    }
+
+    private boolean timeToReduceSize() {
+        return (((size - 1) / (double) (items.length)) < 0.25) || items.length > 15;
+    }
+
     private void resize(int capacity) {
         T[] a = (T[]) new Object[capacity];
         if (removeNextFirst > removeNextLast) {
@@ -32,30 +40,74 @@ public class ArrayDeque<T> {
         removeNextLast = size - 1;
     }
 
+    private void addLastItem(T item, int addIndex) {
+        items[addIndex] = item;
+        size = size + 1;
+        removeNextLast = addIndex;
+    }
+
+    private void addFirstItem(T item, int addIndex) {
+        items[addIndex] = item;
+        size = size + 1;
+        removeNextFirst = addIndex;
+    }
+
+    private void removeLastItem(int removeIndex) {
+        items[removeIndex] = null;
+        size = size - 1;
+        addNextLast = removeIndex;
+    }
+
+    private void removeFirstItem(int removeIndex) {
+        items[removeIndex] = null;
+        size = size - 1;
+        addNextFirst = removeIndex;
+    }
+
+    private T removeToEmpty(int removeIndex) {
+        T removedItem = items[removeIndex];
+        addNextFirst = 0;
+        addNextLast = 1;
+        size = 0;
+        return removedItem;
+    }
+
     public void addLast(T item) {
         if (isEmpty()) {
+            /*
             items[addNextLast] = item;
             size = size + 1;
             removeNextLast = addNextLast;
+             */
+            addLastItem(item, addNextLast);
             removeNextFirst = addNextLast;
             addNextLast = addNextLast + 1;
         } else {
-            if (addNextFirst == addNextLast) {
+            if (timeToIncreaseSize()) {
                 resize(size * 2);
+                /*
                 items[addNextLast] = item;
                 size = size + 1;
                 removeNextLast = addNextLast;
+                 */
+                addLastItem(item, addNextLast);
                 addNextLast = addNextLast + 1;
             } else {
                 if (addNextLast == items.length - 1) {
+                    /*
                     items[addNextLast] = item;
                     size = size + 1;
                     removeNextLast = addNextLast;
+                     */
+                    addLastItem(item, addNextLast);
                     addNextLast = 0;
                 } else {
+                    /*
                     items[addNextLast] = item;
                     size = size + 1;
                     removeNextLast = addNextLast;
+                     */
+                    addLastItem(item, addNextLast);
                     addNextLast = addNextLast + 1;
                 }
             }
@@ -64,28 +116,40 @@ public class ArrayDeque<T> {
 
     public void addFirst(T item) {
         if (isEmpty()) {
+            /*
             items[addNextFirst] = item;
             size = size + 1;
-            removeNextLast = addNextFirst;
             removeNextFirst = addNextFirst;
+             */
+            addFirstItem(item, addNextFirst);
+            removeNextLast = addNextFirst;
             addNextFirst = items.length - 1;
         } else {
-            if (addNextFirst == addNextLast) {
+            if (timeToIncreaseSize()) {
                 resize(size * 2);
+                /*
                 items[addNextFirst] = item;
                 size = size + 1;
                 removeNextFirst = addNextFirst;
+                 */
+                addFirstItem(item, addNextFirst);
                 addNextFirst = addNextFirst - 1;
             } else {
                 if (addNextFirst == 0) {
+                    /*
                     items[addNextFirst] = item;
                     size = size + 1;
                     removeNextFirst = addNextFirst;
+                     */
+                    addFirstItem(item, addNextFirst);
                     addNextFirst = items.length - 1;
                 } else {
+                    /*
                     items[addNextFirst] = item;
                     size = size + 1;
                     removeNextFirst = addNextFirst;
+                     */
+                    addFirstItem(item, addNextFirst);
                     addNextFirst = addNextFirst - 1;
                 }
             }
@@ -97,32 +161,43 @@ public class ArrayDeque<T> {
             return null;
         }
         if (removeNextLast == removeNextFirst) {
+            /*
             T removedItem = items[removeNextLast];
             addNextFirst = 0;
             addNextLast = 1;
             size = 0;
             return removedItem;
+             */
+            return removeToEmpty(removeNextLast);
         }
         T removedItem = items[removeNextLast];
-
-        if ((((size - 1) / (double) (items.length)) >= 0.25) || items.length <= 15) {
-            if (removeNextLast == 0) {
-                items[removeNextLast] = null;
-                size = size - 1;
-                addNextLast = removeNextLast;
-                removeNextLast = items.length - 1;
-            } else {
-                items[removeNextLast] = null;
-                size = size - 1;
-                addNextLast = removeNextLast;
-                removeNextLast = removeNextLast - 1;
-            }
-        } else {
+        if (timeToReduceSize()) {
             resize((int) (items.length * 0.5));
+             /*
             items[removeNextLast] = null;
             size = size - 1;
             addNextLast = removeNextLast;
+             */
+            removeLastItem(removeNextLast);
             removeNextLast = removeNextLast - 1;
+        } else {
+            if (removeNextLast == 0) {
+                /*
+                items[removeNextLast] = null;
+                size = size - 1;
+                addNextLast = removeNextLast;
+                */
+                removeLastItem(removeNextLast);
+                removeNextLast = items.length - 1;
+            } else {
+                /*
+                items[removeNextLast] = null;
+                size = size - 1;
+                addNextLast = removeNextLast;
+                 */
+                removeLastItem(removeNextLast);
+                removeNextLast = removeNextLast - 1;
+            }
         }
         return removedItem;
     }
@@ -132,30 +207,42 @@ public class ArrayDeque<T> {
             return null;
         }
         if (removeNextLast == removeNextFirst) {
+            /*
             T removedItem = items[removeNextFirst];
             addNextFirst = 0;
             addNextLast = 1;
             size = 0;
             return removedItem;
+             */
+            return removeToEmpty(removeNextFirst);
         }
         T removedItem = items[removeNextFirst];
         if ((((size - 1) / (double) (items.length)) >= 0.25) || items.length <= 15) {
             if (removeNextFirst == items.length - 1) {
+                /*
                 items[removeNextFirst] = null;
                 size = size - 1;
                 addNextFirst = removeNextFirst;
+                 */
+                removeFirstItem(removeNextFirst);
                 removeNextFirst = 0;
             } else {
+                /*
                 items[removeNextFirst] = null;
                 size = size - 1;
                 addNextFirst = removeNextFirst;
+                 */
+                removeFirstItem(removeNextFirst);
                 removeNextFirst = removeNextFirst + 1;
             }
         } else {
             resize((int) (items.length * 0.5));
+            /*
             items[removeNextFirst] = null;
             size = size - 1;
             addNextFirst = removeNextFirst;
+             */
+            removeFirstItem(removeNextFirst);
             removeNextFirst = removeNextFirst + 1;
         }
         return removedItem;
