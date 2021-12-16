a4431e49efea8132b0cf3bb7c482d1f06874f0d5
Amanda <manmo431.gmail.com>
2021-12-16 10:07:39 +0000

    finished inheritance4 exercises

---
+++ exercises/inheritance4/ArraySet.java
@@ -0,0 +1,164 @@
+import javax.naming.PartialResultException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+public class ArraySet<T> implements Iterable<T> {
+    private T[] items;
+    private int size;
+
+    public ArraySet() {
+        items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    /* Returns true if this map contains a mapping for the specified key.
+     */
+    public boolean contains(T x) {
+        for (int i = 0; i < size; i += 1) {
+            if (items[i].equals(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /* Associates the specified value with the specified key in this map.
+       Throws an IllegalArgumentException if the key is null. */
+    public void add(T x) {
+        if (x == null) {
+            throw new IllegalArgumentException("can't add null");
+        }
+        if (contains(x)) {
+            return;
+        }
+        items[size] = x;
+        size += 1;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    public int size() {
+        return size;
+    }
+
+    /** returns an iterator (a.k.a. seer) into ME */
+    public Iterator<T> iterator() {
+        return new ArraySetIterator();
+    }
+
+    private class ArraySetIterator implements Iterator<T> {
+        private int wizPos;
+
+        public ArraySetIterator() {
+            wizPos = 0;
+        }
+
+        public boolean hasNext() {
+            return wizPos < size;
+        }
+
+        public T next() {
+            T returnItem = items[wizPos];
+            wizPos += 1;
+            return returnItem;
+        }
+    }
+
+    /*
+    @Override
+    public String toString() {
+        StringBuilder returnSB = new StringBuilder("{");
+        for (int i = 0; i < size - 1; i += 1) {
+            returnSB.append(items[i].toString());
+            returnSB.append(", ");
+        }
+        returnSB.append(items[size - 1]);
+        returnSB.append("}");
+        return returnSB.toString();
+    } */
+
+    @Override
+    public String toString() {
+        List<String> listOfItems = new ArrayList<>();
+        for (T x : this) {
+            listOfItems.add(x.toString());
+        }
+        return "{" + String.join(", ", listOfItems) + "}";
+    }
+
+    public static <T> ArraySet<T> of(T... stuff) {
+        ArraySet<T> returnSet = new ArraySet<>();
+        for (T x : stuff) {
+            returnSet.add(x);
+        }
+        return returnSet;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+        ArraySet<T> o = (ArraySet<T>) other;
+        if (o.size() != this.size()) {
+            return false;
+        }
+        for (T item : this) {
+            if (!o.contains(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ArraySet<String> s = new ArraySet<>();
+        s.add("horse");
+        s.add("fish");
+        s.add("house");
+        s.add("fish");
+        System.out.println(s.contains("horse"));
+        System.out.println(s.size());
+
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+
+        //iteration
+        for (String i : s) {
+            System.out.println(i);
+        }
+
+        //toString
+        System.out.println(aset);
+
+        //equals
+        ArraySet<Integer> aset2 = new ArraySet<>();
+        aset2.add(5);
+        aset2.add(23);
+        aset2.add(42);
+
+        System.out.println(aset.equals(aset2));
+        System.out.println(aset.equals(null));
+        System.out.println(aset.equals("fish"));
+        System.out.println(aset.equals(aset));
+
+        ArraySet<String> asetOfString = ArraySet.of("hi", "I'm", "here");
+        System.out.println(asetOfString);
+    }
+
+    /* Also to do:
+    1. Make ArraySet implement the Iterable<T> interface.
+    2. Implement a toString method.
+    3. Implement an equals() method.
+    */
+}
