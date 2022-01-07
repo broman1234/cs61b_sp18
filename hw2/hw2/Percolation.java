509f2febb6c0df6cba78cb044c785cacca79236e
Amanda <manmo431.gmail.com>
2022-01-07 12:01:42 +0000

    finished hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,119 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+import javax.swing.plaf.synth.SynthLookAndFeel;
 
+public class Percolation {
+    private int numOfOpenSites = 0;
+    private final int size;
+    private final int visualTop;
+    private final int visualBottom;
+    private final boolean[][] sites;
+    private WeightedQuickUnionUF disjointSites;
+
+    private int xyTo1d(int x, int y) {
+        if (x < 0 || x >= size || y < 0 || y >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return size * x + y;
+    }
+
+    public Percolation(int N) {
+        size = N;
+        visualTop = size * size + 1;
+        visualBottom = size * size;
+        sites = new boolean[size][size];
+        disjointSites = new WeightedQuickUnionUF(size * size + 2);
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                sites[i][j] = false;
+            }
+        }
+        for (int i = 0, j = 0; j < N; j += 1) {
+            disjointSites.union(xyTo1d(i, j), visualTop);
+        }
+        for (int i = N - 1, j = 0; j < N; j += 1) {
+            disjointSites.union(xyTo1d(i, j), visualBottom);
+        }
+    }
+
+    private boolean inBoundary(int row, int col) {
+        return row >= 0 && row < size && col >= 0 && col < size;
+    }
+
+    private void unionNeighbor(int row, int col, int nRow, int nCol) {
+        if (inBoundary(nRow, nCol)) {
+            if (isOpen(nRow, nCol)) {
+                disjointSites.union(xyTo1d(row, col), xyTo1d(nRow, nCol));
+            }
+        }
+    }
+
+    public void open(int row, int col) {
+        if (!inBoundary(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        sites[row][col] = true;
+        int upRow= row - 1;
+        int upCol = col;
+        unionNeighbor(row, col, upRow, upCol);
+
+        int downRow = row + 1;
+        int downCol = col;
+        unionNeighbor(row, col, downRow, downCol);
+
+        int leftRow = row;
+        int leftCol = col - 1;
+        unionNeighbor(row, col, leftRow, leftCol);
+
+        int rightRow = row;
+        int rightCol = col + 1;
+        unionNeighbor(row, col, rightRow, rightCol);
+
+        numOfOpenSites += 1;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (!inBoundary(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return sites[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (!inBoundary(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return disjointSites.connected(xyTo1d(row, col), visualTop);
+    }
+
+    public int numberOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    public boolean percolates() {
+        return disjointSites.connected(visualTop, visualBottom);
+    }
+
+    public static void main(String[] args) {
+        Percolation sample = new Percolation(5);
+        sample.open(3, 4);
+        sample.open(2, 4);
+        System.out.println(sample.disjointSites.connected(sample.xyTo1d(3, 4), sample.xyTo1d(2, 4)));
+
+        sample.open(2, 2);
+        sample.open(2, 3);
+        System.out.println(sample.disjointSites.connected(12, 19));
+
+        sample.open(0, 2);
+        sample.open(1, 2);
+        System.out.println(sample.isFull(2, 2));
+
+        sample.open(4, 4);
+        System.out.println(sample.percolates());
+        System.out.println(sample.numberOfOpenSites());
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,89 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
 
+public class PercolationStats {
+    double mean;
+    double stddev;
+    double confidenceLow;
+    double confidenceHigh;
+
+    private double eachExperiment(int N, PercolationFactory pf) {
+        Percolation sample = pf.make(N);
+        int row, col;
+        while (!sample.percolates()) {
+            row = StdRandom.uniform(0, N);
+            col = StdRandom.uniform(0, N);
+            if (sample.isOpen(row, col)) {
+                continue;
+            }
+            sample.open(row, col);
+        }
+        double threshold = sample.numberOfOpenSites() / (double) (N * N);
+        return threshold;
+    }
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        double[] experiments = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            experiments[i] = eachExperiment(N, pf);
+        }
+        mean = StdStats.mean(experiments);
+        stddev = StdStats.stddev(experiments);
+        confidenceLow = mean - 1.96 * stddev / Math.sqrt(T);
+        confidenceHigh = mean + 1.96 * stddev / Math.sqrt(T);
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return confidenceLow;
+    }
+
+    public double confidenceHigh() {
+        return confidenceHigh;
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+
+        Stopwatch timer1 = new Stopwatch();
+        PercolationStats stats1 = new PercolationStats(100, 30, pf);
+        double threshold1 = stats1.mean();
+        stats1.stddev();
+        stats1.confidenceLow();
+        stats1.confidenceHigh();
+        double time1 = timer1.elapsedTime();
+        StdOut.printf("%e (%.2f seconds)\n", threshold1, time1);
+
+        Stopwatch timer2 = new Stopwatch();
+        PercolationStats stats2 = new PercolationStats(200, 30, pf);
+        double threshold2 = stats2.mean();
+        stats2.stddev();
+        stats2.confidenceLow();
+        stats2.confidenceHigh();
+        double time2 = timer2.elapsedTime();
+        StdOut.printf("%e (%.2f seconds)\n", threshold2, time2);
+
+        Stopwatch timer3 = new Stopwatch();
+        PercolationStats stats3 = new PercolationStats(100, 60, pf);
+        double threshold3 = stats3.mean();
+        stats3.stddev();
+        stats3.confidenceLow();
+        stats3.confidenceHigh();
+        double time3 = timer3.elapsedTime();
+        StdOut.printf("%e (%.2f seconds)\n", threshold3, time3);
+    }
 }
